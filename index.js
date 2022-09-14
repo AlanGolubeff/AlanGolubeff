7591bd7aba240d7348e870facd91ba9b9df0793d
AlanGolubeff <alangolu99@gmail.com>
2022-09-14 11:51:28 -0300

    prmera entrega proyecto js

---
+++ index.js
@@ -0,0 +1,28 @@
+class Producto {
+    constructor(id, nombre, precio, categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = parseInt(precio);
+        this.categoria = categoria;
+    }
+}
+const productosId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const nombres = ["remera", "carlo", "campera", "pantalon", "zapatillas", "gorra", "buzo", "camisa", "pantaloneta", "remera", "campera", "pantalon", "zapatillas", "gorra", "buzo", "camisa", "pantaloneta", "remera", "campera", "pantalon"];
+
+const categoria = ["hombre", "mujer", "nino", "nina", "calzado", "accesorios"];
+
+const alala = [];
+
+for (let productoS of productosId) {
+    alala.push(new Producto(productosId[productoS], nombres[Math.floor(Math.random() * 20)], Math.floor(Math.random() * 1000), categoria[Math.floor(Math.random() * 6)]));
+}
+
+function elegir() {
+    let productoelegido = prompt("ingresar producto")
+    console.log(alala[parseInt(productoelegido)])
+}
+for (let index = 0; index < 5; index++) {
+    elegir();
+}
+
